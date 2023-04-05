4553faaa9a445a6f98ac63ae9b8d03aa2672cbc9
kmaxii <esst936@gmail.com>
2023-04-05 15:12:29 +0200

    Adding mario cart drifting, fixed map colliders and replaced kart

--- Machine Learning Game/Assets/CarController.cs
+++ Machine Learning Game/Assets/CarController.cs
@@ -1,3 +1,4 @@
+using DefaultNamespace;
 using UnityEngine;
 
 public class CarController : MonoBehaviour
@@ -6,40 +7,59 @@ public class CarController : MonoBehaviour
     [SerializeField] private Vector3 offset;
 
 
-    [Header("Car stats")] [SerializeField] private float accelerationSpeed = 50;
+    [Header("Car stats")] 
+    [SerializeField] private float accelerationSpeed = 50;
     [SerializeField] private float steeringPower = 18f;
-    [SerializeField] private float driftSteeringPower = 10f;
     [SerializeField] private float gravity = 11f;
 
     [Header("RotateToGround")] [SerializeField]
     private LayerMask raycastLaterMask;
 
     [SerializeField] private float raycastDistance;
-    [SerializeField] private float rotateToGroundSpeed = 7.5f;
+    
+    [Tooltip("The higher the faster")]
+    [SerializeField] private float rotateToGroundSpeed = 4f;
+    
+    [Header("Drifting Settings")]
+    [SerializeField] private float driftSteeringPower = 8f;
+    [SerializeField] private float driftDrag = 1f;
+    [SerializeField] private float driftAccelerationSpeed = 17f;
+    [SerializeField] private float miniDriftEarnedPerSecond = 5f;
+    [SerializeField] private float maxDriftEarnedPerSecond = 23f;
+    [SerializeField] private float driftBoostDuration = 1f;
+    [SerializeField] private AnimationCurve driftBoostCurve;
 
-    private float _currentSpeed;
+    [Header("Punishments")] 
+    [SerializeField] private float offRoadSpeedChange = -24f;
+    
 
-    private float _driftDir;
+    private float _currentSpeed, _defaultDrag, _steer, _driftPower, _boostTimeElapsed;
+    
+    private int _driftDir;
     private bool StartedDrift => Input.GetButtonDown("Jump") && Input.GetAxis("Horizontal") != 0 && !_isDrifting;
-    private bool IsDrifting => Input.GetButton("Jump");
-    private bool EndedDrift => Input.GetButtonUp("Jump");
+    private static bool IsHoldingDriftButton => Input.GetButton("Jump");
+    private static bool EndedDrift => Input.GetButtonUp("Jump");
 
     private bool _isDrifting;
+    
+    private float _driftBoost;
 
-    private float _steer;
+    private Transform _lastRaycastHit;
 
     // Start is called before the first frame update
     void Start()
     {
+        _defaultDrag = sphere.drag;
+        
+        RotateToGroundNormal();
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.parent.position = sphere.transform.position + offset;
-
-
-        _currentSpeed = accelerationSpeed * Input.GetAxis("Vertical");
+        
+        _currentSpeed = _isDrifting ? driftAccelerationSpeed * Input.GetAxis("Vertical") : accelerationSpeed * Input.GetAxis("Vertical");
 
         if (Input.GetAxis("Horizontal") != 0)
             _steer = Input.GetAxis("Horizontal") * steeringPower;
@@ -54,7 +74,18 @@ public class CarController : MonoBehaviour
 
     private void FixedUpdate()
     {
-        sphere.AddForce(transform.forward * _currentSpeed, ForceMode.Acceleration);
+        float fromBoost = 0;
+        if (_boostTimeElapsed < driftBoostDuration)
+        {
+            float t = _boostTimeElapsed / driftBoostDuration;
+            fromBoost = driftBoostCurve.Evaluate(t) * _driftBoost;
+            _boostTimeElapsed += Time.deltaTime;
+        }
+
+        float groundSpeedChange = GetGroundSpeedChange();
+
+        var forward = transform.forward;
+        sphere.AddForce(forward * _currentSpeed + fromBoost * forward + groundSpeedChange * forward, ForceMode.Acceleration);
     }
 
     private void HandleRotation()
@@ -70,41 +101,55 @@ public class CarController : MonoBehaviour
     {
         if (EndedDrift)
         {
-            _isDrifting = false;
+            EndDrift();
             return;
         }
         
-        if (!IsDrifting )
+        if (!IsHoldingDriftButton )
             return;
         
         if (StartedDrift)
             StartDrift();
-        
-        
-        Debug.Log("Is drifting");
+        else if (!_isDrifting)
+            return;
 
-        
-        float currentDriftPower = Input.GetAxis("Horizontal") < 0 ? -Input.GetAxis("Horizontal") : Input.GetAxis("Horizontal");
 
-        _steer = driftSteeringPower * _driftDir * currentDriftPower;
+        float horizontalInput = Input.GetAxis("Horizontal");
+        
+        float currentDriftPower = (_driftDir == 1) ? horizontalInput.Map( -1, 1, 0, 2) : horizontalInput.Map(-1, 1, 2, 0);
+       _driftPower += (_driftDir == 1) 
+           ? horizontalInput.Map( -1, 1, miniDriftEarnedPerSecond, maxDriftEarnedPerSecond) * Time.deltaTime
+           : horizontalInput.Map(-1, 1, maxDriftEarnedPerSecond, miniDriftEarnedPerSecond) * Time.deltaTime;
+
+         _steer = driftSteeringPower * _driftDir * currentDriftPower;
     }
 
+    private void EndDrift()
+    {
+        _isDrifting = false;
+        sphere.drag = _defaultDrag;
+
+        _driftBoost = _driftPower;
+        _boostTimeElapsed = 0f;
+    }
+    
     private void StartDrift()
     {
         //Horizontal is somewhere between -1 and 1, to make it be between 0 and 2 we add 1
-
         _driftDir = Input.GetAxis("Horizontal") < 0 ? -1 : 1;
 
         _isDrifting = true;
+
+        _driftPower = 0f;
+        
+        sphere.drag = driftDrag;
     }
 
     private void RotateToGroundNormal()
     {
-        // cast a ray downwards to get the normal of the ground below the object
-
+        //Cast a ray downwards to get the normal of the ground below the object
         if (Physics.Raycast(transform.position, -Vector3.up, out var hit, raycastDistance, raycastLaterMask))
         {
-            Debug.Log("Raycast hit");
             // calculate the rotation needed to match the ground normal
             var transform1 = transform;
             var rotation = transform1.rotation;
@@ -113,6 +158,16 @@ public class CarController : MonoBehaviour
             // smoothly rotate the object towards the target rotation
             rotation = Quaternion.Lerp(rotation, targetRotation, rotateToGroundSpeed * Time.deltaTime);
             transform.rotation = rotation;
+
+            _lastRaycastHit = hit.transform;
         }
+    }
+
+    private float GetGroundSpeedChange()
+    {
+        if (_lastRaycastHit.transform.CompareTag("Road"))
+            return 0f;
+
+        return offRoadSpeedChange;
     }
 }
---
+++ Machine Learning Game/Assets/FloatExtensions.cs
@@ -0,0 +1,13 @@
+﻿namespace DefaultNamespace
+{
+    public static class FloatExtensions
+    {
+        
+        //This method was taken from: https://forum.unity.com/threads/re-map-a-number-from-one-range-to-another.119437/
+        public static float Map(this float value, float inputFrom, float inputTo, float outputFrom, float outputTo)
+        {
+            return (value - inputFrom) / (inputTo - inputFrom) * (outputTo - outputFrom) + outputFrom;
+        }
+    }
+
+}
